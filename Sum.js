30e604bf50c4eea6c722d17abac78febe932a5e7
S-doremon <72343205+S-doremon@users.noreply.github.com>
2024-06-19 18:54:27 +0530

    Update Sum.js

--- Sum.js
+++ Sum.js
@@ -1,4 +1,4 @@
-//How to find sum of all elements in given array usin reduce() method.
+//How to find sum of all elements in given array using reduce() method.
 
 const arrNum =[1,6,3,8,4,9,5,16]
 const sumOfNum =(arr)=>{
